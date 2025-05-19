600340a1f08c2eaa7d406b89d22875c076864c69
ramprasadsuthi <ramprasadsuthi@gmail.com>
2025-05-19 16:32:08 +0530

    Initial commit

---
+++ src/main/java/com/medha/demoSprintBoot/DemoSprintBootApplication.java
@@ -0,0 +1,13 @@
+package com.medha.demoSprintBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSprintBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSprintBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medha/demoSprintBoot/DemoSprintBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medha.demoSprintBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSprintBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
